526c175750acede2c2b9d96fbf1d0104272681a4
Jonathan Pham <jonpham@users.noreply.github.com>
2024-10-21 18:07:32 -0700

    feat(verification): Update storybook and Root story with preferences

---
+++ src/pages/Root/__stories__/Root.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, within } from '@storybook/test';
+
+import { Root } from '../Root';
+
+const meta = {
+  title: 'Pages/Root',
+  component: Root,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Root>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const LoggedIn: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const header = canvas.getByText(/Vite \+ React/i, { selector: 'h1' });
+    await expect(header).toBeVisible();
+    const countButton = canvas.getByRole('button', { name: /count is 0/ })
+    await expect(countButton).toBeVisible();
+    await expect(countButton).toBeEnabled();
+    await userEvent.click(countButton);
+    await expect(canvas.getByRole('button', { name: /count is 1/ })).toBeVisible();
+  },
+};
