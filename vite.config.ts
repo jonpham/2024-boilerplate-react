a6dcf572a002710fba0d5f6d13bf30e16e7370b1
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-05 15:01:49 -0800

    feat(env): Add src @ module alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config';
+// Allows alias configs to use tsconfig as source of truth
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
+    environment: 'jsdom',
     globals: true,
-    environment: 'jsdom'
+    setupFiles: ['./vitest.setup.ts'],
   }
 })
