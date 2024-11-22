0b79b58870f7db19feba1531f4e27cfd9b98dd5b
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-22 00:50:40 -0800

    feat(dep): Add Tailwind CSS

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,7 +1,12 @@
 import './HelloWorld.css';
 
 function HelloWorld() {
-  return <div className="hello-world">HelloWorld</div>;
+  return (
+    <div className="hello-world">
+      <h1 className="text-3xl font-bold underline">HelloHeader!</h1>
+      HelloWorld
+    </div>
+  );
 }
 
 export { HelloWorld };
