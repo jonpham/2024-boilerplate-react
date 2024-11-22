d9c9499f4c12549a7d2e2e047cf4d22291c5c2e8
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-21 20:04:15 -0800

    feat(styling): Add initial border css to HelloWorld
    • Also display at end of RootPage

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './Root.css';
+import HelloWorld from '@/components/HelloWorld';
 
 function Root() {
   const [count, setCount] = useState(0);
@@ -28,6 +29,9 @@ function Root() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <HelloWorld />
+      </div>
     </>
   );
 }
