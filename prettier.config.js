b1a61ad49f0a7023625abf46883ecf54c16f0925
Jonathan Pham <jonpham@users.noreply.github.com>
2024-11-22 01:05:20 -0800

    feat(css-prettier): Add tailwind css prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -3,8 +3,10 @@
  * @type {import("prettier").Config}
  */
 const prettierConfig = {
+  plugins: ['prettier-plugin-tailwindcss'],
   semi: true,
   singleQuote: true,
+  tailwindStylesheet: './src/application/style/index.css',
   trailingComma: 'es5',
 };
 
